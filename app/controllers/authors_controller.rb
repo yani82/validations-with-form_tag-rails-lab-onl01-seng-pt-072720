388d6db9ffd3b33b32c4997f9871ef63f390ba36
yani82 <yani.iman1@gmail.com>
2020-12-13 23:44:30 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,9 +9,28 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new 
+    end 
   end
+
+  def edit 
+    @author = Author.find(params[:id])
+  end 
+
+  def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    if @author.valid? 
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :edit 
+    end 
+  end 
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new 
+  end 
+
+  def create
+    @post = Post.new
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :new 
+    end 
+  end 
+
   def show
     @post = Post.find(params[:id])
   end
@@ -9,10 +24,13 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end
   end
 
   private
